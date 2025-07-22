1cfd62683a157c722376b2de9ef9019bdcff6ca7
Lussuru <Lussuru17@mail.ru>
2025-07-23 00:07:58 +0300

    исправление ошибки

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import PointPresenter from '../presenter/point-presenter.js';
-const filterContainer = document.querySelector('..trip-controls__filters');
-const contentContainer = document.querySelector('..trip-events');
+import PointPresenter from './presenter/point-presenter.js';
+const filterContainer = document.querySelector('.trip-controls__filters');
+const contentContainer = document.querySelector('.trip-events');
 
 const pointPresenter = new PointPresenter({
   filterContainer: filterContainer,
